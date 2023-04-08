a63fc8fcdd367ec96a998119a2da584f0dbaf2a8
Ekam Lally <Elally7172@conestogac.on.ca>
2023-04-08 03:17:04 -0400

    Added a  loop to the program so that user decides when it closes, fixed the if statements with the use of else if. - Ekam

--- Intro/Source.c
+++ Intro/Source.c
@@ -1,50 +1,56 @@
 
 #include "BusProperties.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    Reservation reservations[MAX_SEATS] = { 0 };
-    char name[50], phone_number[20];
-    int menuChoice;
-    printf("Welcome to the bus planner program\n");
-    printf("select the option that applys to what you want to do.\n");
-    printf("1. Book a seat\n");
-    printf("2. Relocate a seat\n");
-    printf("3. Cancel a reservation\n");
-    printf("4. Exit the program\n");
-    scanf_s("%d", &menuChoice);
+    bool programLoop = true;
+    while (programLoop)
+    {
+        Reservation reservations[MAX_SEATS] = { 0 };
+        char name[50], phone_number[20];
+        int menuChoice;
+        printf("Welcome to the bus planner program\n");
+        printf("select the option that applys to what you want to do.\n");
+        printf("1. Book a seat\n");
+        printf("2. Relocate a seat\n");
+        printf("3. Cancel a reservation\n");
+        printf("4. Exit the program\n");
+        scanf_s("%d", &menuChoice);
 
-    if (menuChoice == 1)
-    {
-        int bus_choice = get_bus_choice();
-        if (bus_choice == 0) {
-            return 1;
+        if (menuChoice == 1)
+        {
+            int bus_choice = get_bus_choice();
+            if (bus_choice == 0) {
+                return 1;
+            }
+            int seat_choice = get_seat_choice(bus_choice, reservations);
+            if (seat_choice == 0) {
+                return 1;
+            }
+            get_user_input(name, phone_number);
+            Reservation reservation = create_reservation(bus_choice, seat_choice, name, phone_number, reservations);
+            save_reservation(reservation);
+            confirm_reservation(reservation);
+            display_reservation_details(reservation);
         }
-        int seat_choice = get_seat_choice(bus_choice, reservations);
-        if (seat_choice == 0) {
-            return 1;
+        else if (menuChoice == 2)
+        {
+            relocate_user(reservations);
         }
-        get_user_input(name, phone_number);
-        Reservation reservation = create_reservation(bus_choice, seat_choice, name, phone_number, reservations);
-        save_reservation(reservation);
-        confirm_reservation(reservation);
-        display_reservation_details(reservation);
-    }
-    if (menuChoice == 2)
-    {
-        relocate_user(reservations);
-    }
-    if (menuChoice == 3)
-    {
-        // code to cancel a seat.
-    }
-    if (menuChoice == 4)
-    {
-        exit(1);
-    }
-    else 
-    {
-        printf("not a valid option.");
+        else if (menuChoice == 3)
+        {
+            // code to cancel a seat.
+        }
+        else if (menuChoice == 4)
+        {
+            programLoop = false;
+            exit(1);
+        }
+        else
+        {
+            printf("not a valid option.");
+        }
     }
     return 0;
 }
