bd87b222eff3510090c2d9f0b8bd01aa392ead62
Aidan <anickerson8778@conestogac.on.ca>
2023-04-08 20:34:26 -0400

    Revert "Modified the create_ reservations function, added a relocation function and a display_new_reservation_details function. modified the main.c file and added switch cases for when the user finishes booking. added a cancel reservation option but it does not work properly"
    This reverts commit cd059966f8320b68ee0512f35925a200f44b9488.

--- Intro/Source.c
+++ Intro/Source.c
@@ -11,26 +11,8 @@ int main() {
     get_user_input(name, phone_number);
     Reservation reservation = create_reservation(bus_choice, name, phone_number, reservations);
     save_reservation(reservation);
-    printf("Enter 1 to cancel reservation, enter 2 to relocate your seat or any other number to exit: \n\n");
-    do {
-        int new_choice;
-        printf("Enter 1 to cancel a reservation\n\n");
-        printf("Enter 2 to relocate a reservation\n\n");
-        printf("Enter 3 to exit\n\n");
-        scanf("%d", &new_choice);
-        switch (new_choice) {
-        case 1:
-            cancel_reservation(reservations, reservations);
-            break;
-        case 2:
-            relocate_user(reservations);
-            break;
-        case 3:
-            exit(0);
-            break;
-        default:
-            printf("Invalid choice. Please try again.\n");
-        }
-    } while (1);
+    confirm_reservation(reservation);
+    display_reservation_details(reservation);
+    relocate_user(reservations);
     return 0;
 }
